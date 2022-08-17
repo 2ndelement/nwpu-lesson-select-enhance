6262df2958e38c92ccfed015fa89d55e20a330a5
2ndelement <72551339+2ndelement@users.noreply.github.com>
2022-08-17 20:51:01 +0800

    Create nwpu-lesson-select-enhance.js

---
+++ nwpu-lesson-select-enhance.js
@@ -0,0 +1,65 @@
+// ==UserScript==
+// @name         翱翔门户选课增强
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  翱翔门户选课增强，显示已选人数
+// @author       2ndelement
+// @match        https://jwxt.nwpu.edu.cn/course-selection/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=nwpu.edu.cn
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+
+(function () {
+  function run() {
+    "use strict";
+    var reg =
+      /https:\/\/jwxt.nwpu.edu.cn\/course-selection\/#\/course-select\/(\d+)\/turn\/(\d+)\/select/;
+    var groups = reg.exec(window.location.href);
+
+    var studentId = groups[1];
+    var turnId = groups[2];
+
+    var token = document.cookie.match(/token=([^;]+)/)[1];
+    var getCourses = new XMLHttpRequest();
+    getCourses.open(
+      "GET",
+      "https://jwxt.nwpu.edu.cn/course-selection-api/api/v1/student/course-select/selected-lessons/" +
+        turnId +
+        "/" +
+        studentId,
+      false
+    );
+    getCourses.setRequestHeader("Authorization", token);
+    getCourses.send(null);
+    var courses = JSON.parse(getCourses.responseText)["data"];
+    var t = document.querySelectorAll("table.el-table__body")[0];
+    var coursesDom = t.querySelectorAll("tr.el-table__row");
+    for (var i = 0; i < coursesDom.length; i++) {
+      var courseName = coursesDom[i].outerText.match(/(.*?)[\nU]/)[1].trim();
+      var currentCourse = courses.find(
+        (item) => item["course"]["nameZh"] == courseName
+      );
+      var pannel = coursesDom[i].querySelector(
+        "div.control-label.pinned-label"
+      );
+      if (!pannel) {
+      	continue;
+      	}
+      
+      var getSelected = new XMLHttpRequest();
+      getSelected.open(
+        "GET",
+        "https://jwxt.nwpu.edu.cn/student/for-std-lessons/info/" +
+          currentCourse["id"],
+        false
+      );
+      getSelected.send(null);
+      groups = getSelected.responseText.match(
+        /<th width="11%">已选学生数<\/th>\n.*?<td width="12%">(\d+)<\/td>\n.*?<th width="10%">待释放保留人数<\/th>\n.*?<td width="12%"><span>(\d+)<\/span><\/td>\n.*?\n.*?\n.*?<th>选课人数上限<\/th>\n.*?<td>(\d+)<\/td?/
+      );
+      pannel.innerText = groups[1] + "/" + groups[3];
+    }
+  }
+  setTimeout(run, 500);
+})();
