6c77c421827c5f0f457e6da1640fcd83230b7acd
2ndelement <72551339+2ndelement@users.noreply.github.com>
2022-08-19 01:02:19 +0800

    添加进度条
    添加进度条

--- nwpu-lesson-select-enhance.user.js
+++ nwpu-lesson-select-enhance.user.js
@@ -15,6 +15,19 @@
 // ==/UserScript==
 
 (function () {
+  var paser = new DOMParser();
+  function updateBar(innerBar, selected, limit) {
+    let color = selected - limit > 0 ? "rgb(255, 107, 107)" : "rgb(6, 86, 139)";
+    let width = (selected * 100) / limit + "%";
+    width = width > 100 ? 100 : width;
+    innerBar.style.backgroundColor = color;
+    innerBar.style.width = width;
+  }
+  function createProcessBar() {
+    let str =
+      '<div data-v-7ed17b25="" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" class="el-progress el-progress--line el-progress--without-text"><div class="el-progress-bar"><div class="el-progress-bar__outer" style="height: 6px;"><div class="el-progress-bar__inner" style="width: 100%; background-color: rgb(255, 107, 107);"><!----></div></div></div><!----></div>';
+    return paser.parseFromString(str, "text/html").querySelector("div");
+  }
   function run() {
     "use strict";
     var table = document.querySelectorAll("table.el-table__body")[0];
@@ -60,33 +73,42 @@
               simpleCourses[key].selected = parseInt(
                 body2.data[key].split("-")[0]
               );
-              for (let i = 0; i < coursesDom.length; i++) {
-                var pannel = coursesDom[i].querySelector(
-                  "div.control-label.pinned-label"
-                );
-                if (!pannel) {
-                  continue;
-                }
-                var courseName = coursesDom[i].outerText
-                  .match(/(.*?)[\nU]/)[1]
-                  .trim();
-                var currentCourse;
-                for (let key in simpleCourses) {
-                  if (simpleCourses[key].name == courseName) {
-                    currentCourse = simpleCourses[key];
-                  }
-                }
-                var overNum = currentCourse.selected - currentCourse.limit;
-                var color = overNum > 0 ? "red" : "green";
-                pannel.innerHTML =
-                  "<font color=" +
-                  color +
-                  ">" +
-                  currentCourse.selected +
-                  "</font>" +
-                  "/" +
-                  currentCourse.limit;
+            }
+            for (let i = 0; i < coursesDom.length; i++) {
+              var pannel = coursesDom[i].querySelector(
+                "div.control-label.pinned-label"
+              );
+              if (!pannel) {
+                continue;
               }
+              var bar = coursesDom[i].querySelector(
+                "div.el-progress-bar__inner"
+              );
+              if (!bar) {
+                pannel.parentElement.appendChild(createProcessBar());
+                bar = coursesDom[i].querySelector("div.el-progress-bar__inner");
+              }
+              var courseName = coursesDom[i].outerText
+                .match(/(.*?)[\nU]/)[1]
+                .trim();
+              var currentCourse;
+              for (let key in simpleCourses) {
+                if (simpleCourses[key].name == courseName) {
+                  currentCourse = simpleCourses[key];
+                  break;
+                }
+              }
+              var overNum = currentCourse.selected - currentCourse.limit;
+              var color = overNum > 0 ? "red" : "green";
+              updateBar(bar, currentCourse.selected, currentCourse.limit);
+              pannel.innerHTML =
+                "<font color=" +
+                color +
+                ">" +
+                currentCourse.selected +
+                "</font>" +
+                "/" +
+                currentCourse.limit;
             }
           });
       });
