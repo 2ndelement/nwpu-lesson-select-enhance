f4d057843e02cde9657aaf664a9bb40db665a2c2
2ndelement <72551339+2ndelement@users.noreply.github.com>
2022-08-18 18:26:38 +0800

    resolved #3
    使用了效率更高的api,优化了请求逻辑,启用实时刷新

--- nwpu-lesson-select-enhance.user.js
+++ nwpu-lesson-select-enhance.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         翱翔门户选课增强
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  翱翔门户选课增强，显示已选人数
 // @author       2ndelement
 // @match        https://jwxt.nwpu.edu.cn/course-selection/
@@ -17,55 +17,79 @@
 (function () {
   function run() {
     "use strict";
-    var urlReg =
+    var table = document.querySelectorAll("table.el-table__body")[0];
+    var coursesDom = table.querySelectorAll("tr.el-table__row");
+    const urlReg =
       /https:\/\/jwxt.nwpu.edu.cn\/course-selection\/#\/course-select\/(\d+)\/turn\/(\d+)\/select/;
     var groups = urlReg.exec(window.location.href);
-
-    var studentId = groups[1];
-    var turnId = groups[2];
-
-    var token = document.cookie.match(/token=([^;]+)/)[1];
-    var getCourses = new XMLHttpRequest();
-    getCourses.open(
-      "GET",
+    const studentId = groups[1];
+    const turnId = groups[2];
+    const token = document.cookie.match(/token=([^;]+)/)[1];
+    const selectedLessonApi =
       "https://jwxt.nwpu.edu.cn/course-selection-api/api/v1/student/course-select/selected-lessons/" +
-        turnId +
-        "/" +
-        studentId,
-      false
-    );
-    getCourses.setRequestHeader("Authorization", token);
-    getCourses.send(null);
-    var courses = JSON.parse(getCourses.responseText)["data"];
-    var t = document.querySelectorAll("table.el-table__body")[0];
-    var coursesDom = t.querySelectorAll("tr.el-table__row");
-    for (var i = 0; i < coursesDom.length; i++) {
-      var courseName = coursesDom[i].outerText.match(/(.*?)[\nU]/)[1].trim();
-      var currentCourse = courses.find(
-        (item) => item["course"]["nameZh"] == courseName
-      );
-      var pannel = coursesDom[i].querySelector(
-        "div.control-label.pinned-label"
-      );
-      if (!pannel) {
-        continue;
-      }
-      var selectReg =
-        /<th width="11%">已选学生数<\/th>\n.*?<td width="12%">(\d+)<\/td>\n.*?<th width="10%">待释放保留人数<\/th>\n.*?<td width="12%"><span>(\d+)<\/span><\/td>\n.*?\n.*?\n.*?<th>选课人数上限<\/th>\n.*?<td>(\d+)<\/td?/;
-      var getSelected = new XMLHttpRequest();
-      getSelected.open(
-        "GET",
-        "https://jwxt.nwpu.edu.cn/student/for-std-lessons/info/" +
-          currentCourse["id"],
-        false
-      );
-      getSelected.send(null);
-      groups = getSelected.responseText.match(selectReg);
-      var overNum = parseInt(groups[1]) - parseInt(groups[3]);
-      var color = overNum > 0 ? "red" : "green";
-      pannel.innerHTML =
-        "<font color=" + color + ">" + groups[1] + "</font>" + "/" + groups[3];
-    }
+      turnId +
+      "/" +
+      studentId;
+    var lessonStuCountApi =
+      "https://jwxt.nwpu.edu.cn/course-selection-api/api/v1/student/course-select/std-count?lessonIds=";
+    const headers = {
+      Authorization: token,
+    };
+    var courses;
+    var simpleCourses = {};
+
+    fetch(selectedLessonApi, { method: "GET", headers: headers })
+      .then((response) => response.json())
+      .then((body) => {
+        courses = body.data;
+        courses.forEach((course) => {
+          simpleCourses[course.id] = {
+            name: course.course.nameZh,
+            limit: parseInt(course.limitCount),
+          };
+          lessonStuCountApi += course.id + ",";
+        });
+        lessonStuCountApi = lessonStuCountApi.substring(
+          0,
+          lessonStuCountApi.length - 1
+        );
+        fetch(lessonStuCountApi, { method: "GET", headers })
+          .then((response) => response.json())
+          .then((body2) => {
+            for (let key in body2.data) {
+              simpleCourses[key].selected = parseInt(
+                body2.data[key].split("-")[0]
+              );
+              for (let i = 0; i < coursesDom.length; i++) {
+                var pannel = coursesDom[i].querySelector(
+                  "div.control-label.pinned-label"
+                );
+                if (!pannel) {
+                  continue;
+                }
+                var courseName = coursesDom[i].outerText
+                  .match(/(.*?)[\nU]/)[1]
+                  .trim();
+                var currentCourse;
+                for (let key in simpleCourses) {
+                  if (simpleCourses[key].name == courseName) {
+                    currentCourse = simpleCourses[key];
+                  }
+                }
+                var overNum = currentCourse.selected - currentCourse.limit;
+                var color = overNum > 0 ? "red" : "green";
+                pannel.innerHTML =
+                  "<font color=" +
+                  color +
+                  ">" +
+                  currentCourse.selected +
+                  "</font>" +
+                  "/" +
+                  currentCourse.limit;
+              }
+            }
+          });
+      });
   }
-  setTimeout(run, 500);
+  setInterval(run, 1000);
 })();
